876d511d5a1d0614c058996c92027fe259dcb902
DellAce <agentmemov@gmail.com>
2025-05-21 15:32:45 +0300

    fix: исправлены ошибки в функции split_data, улучшена обработка интервалов

--- prog/lab3/splitdata.py
+++ prog/lab3/splitdata.py
@@ -2,26 +2,30 @@ def split_data(content, interval):
     intervals = []
     current_interval = []
     start_time = None
+    prev_time = None
 
     for row in content:
         if not row:
             continue
         try:
             time = float(row[0])
+            value = float(row[1])
             if start_time is None:
                 start_time = time
 
             if time < start_time + interval:
-                current_interval.append(float(row[1]))
+                current_interval.append(value)
             else:
-                intervals.append((start_time, start_time + interval, current_interval))
+                intervals.append((start_time, prev_time, current_interval))
                 start_time = time
-                current_interval = [float(row[1])]
+                current_interval = [value]
+
+            prev_time = time
         except ValueError:
             print(f"Ошибка преобразования строки в число: {row[0]}")
             continue
 
     if current_interval:
-        intervals.append((start_time, start_time + interval, current_interval))
+        intervals.append((start_time, prev_time, current_interval))
 
     return intervals
