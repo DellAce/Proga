169bafc1465252cbffc0662913e2f5d58041ecf7
DellAce <agentmemov@gmail.com>
2025-05-25 19:52:22 +0300

    fix: исправлены сообщения об ошибках и улучшена логика обработки аргументов в main()

--- prog/lab4/lab4.py
+++ prog/lab4/lab4.py
@@ -8,24 +8,24 @@ lib.calculate_primes.restype = None
 
 def main():
     if len(sys.argv) != 3:
-        print("Usage: python lab4_1.py <start> <end>")
-        return
+        print("python lab4_1.py <start> <end>")
+        sys.exit(1)
     start = int(sys.argv[1])
     end = int(sys.argv[2])
     if start < 4 or end < 4 or start > end or start % 2 != 0 or end % 2 != 0:
         print("Неверные аргументы, необходимо 2 четных числа >= 4 и старт < конец.")
-        return
+        sys.exit(1)
     n = end + 1
-    ARRAY = c_int * (n)
+    ARRAY = c_int * (n + 1)
     primes = ARRAY()
-    for k in range(start, end + 1):
-        lib.calculate_primes(primes, n)
+    lib.calculate_primes(primes, n)
+    for k in range(start, end + 1, 2):
         normas = []
         for i in range(2, k // 2 + 1):
             if primes[i] == 1 and primes[k - i] == 1:
                 normas.append((i, k - i))
         count = len(normas)
-        min_pair = normas[0] if normas else None
+        min_pair = normas[0]
         print(f"{k} {count} {min_pair}")
 
 
